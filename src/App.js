fcc18ed730e42779f7d91495cf8c14ff86ebcaf6
JulianPerez852 <jdph852@gmail.com>
2019-06-10 19:49:29 -0500

    Commit para evaluacion

--- src/App.js
+++ src/App.js
@@ -4,21 +4,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <hi1>
+        Mi primera aplicación
+      </hi1>
+      <p>
+        Esta es mi primera aplicación en React, está padrísimo!
+      </p>
     </div>
   );
 }
